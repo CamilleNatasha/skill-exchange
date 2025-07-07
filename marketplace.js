cef7ddde3c938f31e197dbc36589ef81ab68d39d
CamilleNatasha <roanstarsha@gmail.com>
2025-07-07 22:18:39 +0300

    Updated page comments

---
+++ marketplace.js
@@ -0,0 +1,4 @@
+// page to display the marketplace of the application
+// Connects to the hmepage.js file
+// Shows the different developers and their skillsets
+// It includes links to the product listing and product details pages.
